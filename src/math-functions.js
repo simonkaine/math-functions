ab93887be64dbc07f42649d0bc56058b25087d78
Simon Kaine <simonkaine@gmail.com>
2021-07-12 09:46:04 -0700

    This is problem 01

--- src/math-functions.js
+++ src/math-functions.js
@@ -12,7 +12,8 @@ example and uses the values that were input into the function:
 */
 
 export function sum(a, b) {
-
+    const c = a + b;
+    return [c, `The sum of ${a} and ${b} is ${c}.`];
 }
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
